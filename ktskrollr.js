f7e0adb055defd6f2789545227ba4dbaa24b0602
fanbeatsman <fan_ma8@hotmail.com>
2015-01-23 13:09:01 -0500

    basic fct works

--- ktskrollr.js
+++ ktskrollr.js
@@ -2,9 +2,11 @@
 	function kt(dictionary){
 		var startd = dictionary.startd || 0;
 		var step = dictionary.step || 200;
+		var kt = 0;
 		String.prototype.splice = function(idx,rem,s){
 			return(this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
 		};
+
 		$(".kt").each(function(){
 			var rawString = $(this).text();
 			var length = rawString.length;
@@ -21,9 +23,27 @@
 				ptr++;
 				batchCtr++;
 			}
+
+
 			console.log(rawString);
 			$(this).html("");
 			$(this).append(rawString);
+
+			var f = parseInt(startd) + step; //f is a var that will loop over the spans and place skrollr data according to the given parameters 
+			var g = f + step + step;
+			$(".kt").each(function(){
+		$(this).children("span").each(function(){
+
+			$(this).attr("data-"+f,"opacity:0;");
+			$(this).attr("data-"+g,"opacity:1;");
+			f = f+step;
+			g = g+step;
+		});
+		f = f+step+step;
+		g = g+step+step;
+});
+		//	$(this).attr("data-"+f,"opacity:0;");
+		//	$(this).attr("data-"+f+step,"opacity:1;");
 		}
 			);
 
